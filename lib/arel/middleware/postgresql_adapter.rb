65a639a66daa9066fa040e4343872069f1db77e3
Tom de Vries <tom@hackerone.com>
2024-06-05 12:11:27 +0200

    Pass additional arguments

--- lib/arel/middleware/postgresql_adapter.rb
+++ lib/arel/middleware/postgresql_adapter.rb
@@ -38,20 +38,20 @@ module Arel
         end
       end
 
-      def exec_no_cache(sql, name, binds, async: false)
+      def exec_no_cache(sql, name, binds, async: false, **additional_arguments)
         Arel::Middleware.current_chain.execute(sql, binds) do |processed_sql, processed_binds|
           Arel::Middleware::Result.create(
-            data: super(processed_sql, name, processed_binds, async: async),
+            data: super(processed_sql, name, processed_binds, async: async, **additional_arguments),
             from: Arel::Middleware::PGResult,
             to: Arel::Middleware::PGResult,
           )
         end
       end
 
-      def exec_cache(sql, name, binds, async: false)
+      def exec_cache(sql, name, binds, async: false, **additional_arguments)
         Arel::Middleware.current_chain.execute(sql, binds) do |processed_sql, processed_binds|
           Arel::Middleware::Result.create(
-            data: super(processed_sql, name, processed_binds, async: async),
+            data: super(processed_sql, name, processed_binds, async: async, **additional_arguments),
             from: Arel::Middleware::PGResult,
             to: Arel::Middleware::PGResult,
           )
